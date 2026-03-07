2717f76fce7705d099439844edf559efd8146582
Vartika Pundeer <vartikapundeer15@gmail.com>
2026-03-07 23:31:16 +0530

    Fix Redis connection using REDIS_URL

--- redisClient.js
+++ redisClient.js
@@ -1,16 +1,29 @@
 const { createClient } = require("redis");
 
-const client = createClient({
-    url: "redis://127.0.0.1:6379"
-});
-
-client.on("error", (err) => {
-    console.log("Redis Error:", err);
-});
+let redisClient;
 
 async function connectRedis() {
-    await client.connect();
-    console.log("Redis Connected");
+    try {
+
+        redisClient = createClient({
+            url: process.env.REDIS_URL || "redis://127.0.0.1:6379"
+        });
+
+        redisClient.on("error", (err) => {
+            console.log("Redis Error:", err);
+        });
+
+        await redisClient.connect();
+
+        console.log("Redis connected successfully");
+
+    } catch (error) {
+        console.log("Redis connection failed:", error);
+    }
 }
 
-module.exports = { client, connectRedis };
+function getRedisClient() {
+    return redisClient;
+}
+
+module.exports = { connectRedis, getRedisClient };
