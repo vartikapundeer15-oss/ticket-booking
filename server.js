248f859027c98b74aff0c63f76178b7cb0285575
Vartika Pundeer <vartikapundeer15@gmail.com>
2026-03-07 23:57:17 +0530

    Add start script for Render

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const { connectRedis } = require("./redisClient");
 const { initializeSeats } = require("./seatController");
 const seatRoutes = require("./seatRoutes");
@@ -7,23 +6,25 @@ const seatRoutes = require("./seatRoutes");
 const app = express();
 
 app.use(express.json());
+app.use("/", seatRoutes);
 
 // Render requires dynamic port
 const PORT = process.env.PORT || 3000;
 
-app.use("/", seatRoutes);
-
 async function startServer() {
     try {
-        await connectRedis();
-        await initializeSeats();
 
+        // Start server first so Render detects port
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
 
+        // Then connect Redis
+        await connectRedis();
+        await initializeSeats();
+
     } catch (error) {
-        console.error("Error starting server:", error);
+        console.error("Startup error:", error);
     }
 }
 
